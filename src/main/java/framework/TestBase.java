9c0270213f03fa12acf6655cbdd0e669c184fb5a
jason de silva <jasdes@mybudget.com.au>
2022-06-08 09:21:56 +0530

    Improved the framework and added the test case basic flow

--- src/main/java/framework/TestBase.java
+++ src/main/java/framework/TestBase.java
@@ -1,4 +1,49 @@
 package framework;
 
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.asserts.SoftAssert;
+import util.LoggerUtil;
+import util.PropertyReader;
+
+import static framework.Constants.COLON_DOUBLE_SLASH;
+import static framework.Constants.SINGLE_SLASH;
+import static io.restassured.RestAssured.given;
+
 public class TestBase {
+
+    public static RequestSpecification requestSpecification = null;
+    public SoftAssert softAssert;
+
+    @BeforeClass
+    public void initialiseAssertions() {
+        softAssert = new SoftAssert();
+        LoggerUtil.log("Before class method successfully completed", LogLevels.INFO);
+    }
+
+    @AfterTest
+    public void assertTest() {
+        softAssert.assertAll();
+        LoggerUtil.log("After class method successfully completed", LogLevels.INFO);
+    }
+
+    /**
+     * This method provides the capability create the RequestSpecification which is used to invoke an API.
+     * API protocol, HOST, Version are parameterized and is configured via a property file.
+     * @return RequestSpecBuilder object
+     */
+    @BeforeClass
+    public static RequestSpecification sendApiRequest() {
+
+        requestSpecification = new RequestSpecBuilder()
+                .setBaseUri(PropertyReader.getProperty("API_PROTOCOL") + COLON_DOUBLE_SLASH + PropertyReader.getProperty("API_HOST"))
+                .setBasePath(SINGLE_SLASH + PropertyReader.getProperty("API_VERSION") + SINGLE_SLASH).addHeader("Accept", "application/json")
+                .setRelaxedHTTPSValidation()
+                .build();
+        LoggerUtil.log("RequestSpecBuilder successfully created", LogLevels.INFO);
+        return given().spec(requestSpecification).when().log().all();
+    }
+
 }
--- src/test/java/com/automation/tests/CategoryTest.java
+++ src/test/java/com/automation/tests/CategoryTest.java
@@ -1,4 +1,42 @@
 package com.automation.tests;
 
-public class CategoryTest {
+import framework.TestBase;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static org.apache.http.HttpStatus.SC_OK;
+
+public class CategoryTest extends TestBase {
+
+    /**
+     * API Test to verify,
+     * Name = "Carbon credits"
+     * CanRelist = true
+     * The Promotions element with Name = "Gallery" has a Description that contains the text "Good position in category"
+     */
+    @Test(description = "Test case #1: Verify get request category body")
+    public void validateAcceptanceCriteria() {
+
+        String path ="Categories/6327/Details.json";
+        String expectedName = "Carbon credits";
+        String expectedGallery ="Good position in category";
+
+        Response response = sendApiRequest().param("catalogue", "false").get(path);
+        response.prettyPeek();
+        softAssert.assertEquals(response.getStatusCode(), SC_OK, "Status code is invalid");
+        softAssert.assertEquals(response.jsonPath().getString("Name"),expectedName,"Name did not match "+expectedName);
+        softAssert.assertTrue(response.jsonPath().getBoolean("CanRelist"),"CanRelist value did not match");
+
+        List<Map<String, String>> values = response.jsonPath().getList("Promotions");
+        //todo iterate the map and verify ac #3
+
+
+
+        }
 }
